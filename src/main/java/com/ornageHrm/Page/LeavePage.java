6ded8f2ad1e3555a1fe1960c1520a6b9fa5ff2a9
Joyce-Aliyas <81835247+Joyce-Aliyas@users.noreply.github.com>
2021-05-04 20:14:24 -0400

    recruitment page incomplete (#4)
    * recruitment page incomplete
    
    * recruitment page in progress
    
    * complete test-recruitment page
    
    * modification done as per comments

--- src/main/java/com/ornageHrm/Page/LeavePage.java
+++ src/main/java/com/ornageHrm/Page/LeavePage.java
@@ -8,67 +8,63 @@ import org.openqa.selenium.support.ui.Select;
 import com.orangeHrm.base.TestBase;
 
 public class LeavePage extends TestBase {
-	
-	LoginPage loginPage;
-	DashBoardPage dashBoardPage;
-	
-	@FindBy(id="calFromDate")
+
+	@FindBy(id = "calFromDate")
 	WebElement clickFromDate;
-	
-	@FindBy(id="calToDate")
+
+	@FindBy(id = "calToDate")
 	WebElement clickTodate;
-	
-	@FindBy(css="#ui-datepicker-div tr td:nth-of-type(6) a:nth-child(1)")
+
+	@FindBy(css = "#ui-datepicker-div tr td:nth-of-type(6) a:nth-child(1)")
 	WebElement firstDate;
-	
-	@FindBy(css="#ui-datepicker-div tr:nth-of-type(6) td a")
+
+	@FindBy(css = "#ui-datepicker-div tr:nth-of-type(6) td a")
 	WebElement lastDate;
-	
-	@FindBy(id="leaveList_chkSearchFilter_checkboxgroup_allcheck")
+
+	@FindBy(id = "leaveList_chkSearchFilter_checkboxgroup_allcheck")
 	WebElement checkBoxAll;
-	
-	@FindBy(id="btnSearch")
+
+	@FindBy(id = "btnSearch")
 	WebElement searchButton;
-	
-	@FindBy(css="select[class='ui-datepicker-month']")
+
+	@FindBy(css = "select[class='ui-datepicker-month']")
 	WebElement selectWebElement;
-	
-	
+
 	public LeavePage() {
 		waitForDocumentCompleteState(15);
 		PageFactory.initElements(driver, this);
-		
+
 	}
-	
+
 	public void clickFromDate() {
-		clickFromDate.click();	
+		clickFromDate.click();
 	}
-	
+
 	public void clickToDate() {
 		clickTodate.click();
 	}
-	
+
 	public void clickSelectFromDate() {
 		firstDate.click();
 	}
-	
+
 	public void clickLastDate() {
 		lastDate.click();
 	}
-	
+
 	public void selectCheckBox() {
 		checkBoxAll.click();
 	}
-	
+
 	public void clickSearchButton() {
 		searchButton.click();
 	}
-	
+
 	public void selectAprilMonth() {
-		Select sel=new Select(selectWebElement);
+		Select sel = new Select(selectWebElement);
 		sel.selectByIndex(3);
 	}
-	
+
 	public void searchUsingDate() {
 		clickFromDate();
 		selectAprilMonth();
